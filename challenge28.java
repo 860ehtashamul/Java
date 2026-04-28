a909111274389f64f5c88c7ea1e991633f62ca9a
EHTASHAMUL Haque <ehtashamulhaque860@gmail.com>
2026-04-28 16:11:01 +0530

    Add files via upload

---
+++ challenge28.java
@@ -0,0 +1,14 @@
+public class challenge28{
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("welcome to multiplicaton world");
+        System.out.println("please enter your number");
+        int num=Scanner.nextInt();
+    }
+    public static void multiplicationTable(int num){
+        int i=1;
+        while(i<=10);
+        System.out.println(num + "x" + i + "=");
+        i++;
+    }
+}
